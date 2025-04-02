7517e6e16d2e2a7ff0f4030ea7c336f7957266a0
ilyaxtom <ilyaganak5@gmail.com>
2025-04-02 17:55:10 +0300

    Set React Refresh Plugin

--- config/buildPlugins.ts
+++ config/buildPlugins.ts
@@ -1,11 +1,18 @@
 import { Configuration } from 'webpack';
 import {BuildOptions} from "./types/types";
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
 
-export function buildPlugins({paths}: BuildOptions): Configuration['plugins'] {
+export function buildPlugins({paths, mode}: BuildOptions): Configuration['plugins'] {
+    const isDev = mode === 'development';
+
     const plugins: Configuration['plugins'] = [
-        new HtmlWebpackPlugin({template: paths.html})
+        new HtmlWebpackPlugin({template: paths.html}),
     ];
+
+    if (isDev) {
+        plugins.push(new ReactRefreshWebpackPlugin());
+    }
 
     return plugins;
 }
