a3a01731789ff40c5e51c9967e25b0cb2e7d3bce
ilyaxtom <ilyaganak5@gmail.com>
2025-04-16 19:55:23 +0300

    Update Story for new Select logic

--- src/stories/Select.stories.tsx
+++ src/stories/Select.stories.tsx
@@ -1,94 +1,92 @@
 import { Meta, StoryObj } from '@storybook/react';
-import { ComponentProps } from 'react';
+import { useMemo, useState } from 'react';
 import Select from '@/components/Select';
-
-type StoryProps = ComponentProps<typeof Select>;
+import SelectItem from '@/components/Select/SelectItem';
 
 const options = [
-    { id: 1, title: 'Option 1', isSelected: false },
-    { id: 2, title: 'Option 2', isSelected: false },
-    { id: 3, title: 'Option 3', isSelected: true },
-    { id: 4, title: 'Option 4', isSelected: false },
-    { id: 5, title: 'Option 5', isSelected: false },
-    { id: 6, title: 'Option 6', isSelected: false },
+    { id: 1, title: 'Option 1' },
+    { id: 2, title: 'Option 2' },
+    { id: 3, title: 'Option 3' },
+    { id: 4, title: 'Option 4' },
+    { id: 5, title: 'Option 5' },
+    { id: 6, title: 'Option 6' },
 ];
 
-const meta: Meta<StoryProps> = {
+const meta: Meta = {
     component: Select,
     argTypes: {
-        options: {
-            table: {
-                disable: true,
-            },
-        },
-        selectedId: {
-            table: {
-                disable: true,
-            },
-        },
         variant: {
             options: ['standard', 'filled', 'outlined'],
-            control: {
-                type: 'select',
-            },
+            control: { type: 'select' },
         },
         sz: {
             options: ['small', 'standard'],
-            control: {
-                type: 'select',
-            },
+            control: { type: 'select' },
         },
-    },
-    args: {
-        options,
     },
 };
 
 export default meta;
 
-type Story = StoryObj<StoryProps>;
+type Story = StoryObj;
+
+const Template = (args: any) => {
+    const [selectedId, setSelectedId] = useState<number | null>(null);
+
+    const optionsList = options.map(({ id, title }) => (
+        <SelectItem<number>
+            key={id}
+            id={id}
+            selected={selectedId === id}
+            onChange={setSelectedId}
+        >
+            {title}
+        </SelectItem>
+    ));
+
+    const selectedTitle = useMemo(() => {
+        const option = options.find((o) => o.id === selectedId);
+        return option ? option.title : '';
+    }, [selectedId]);
+
+    return (
+        <Select selectedTitle={selectedTitle} {...args}>
+            {optionsList}
+        </Select>
+    );
+};
 
 export const Outlined: Story = {
+    render: Template,
     args: {
         variant: 'outlined',
-    },
-    render: ({ options, ...args }) => {
-        return <Select options={options} {...args} />;
     },
 };
 
 export const Filled: Story = {
+    render: Template,
     args: {
         variant: 'filled',
-    },
-    render: ({ options, ...args }) => {
-        return <Select options={options} {...args} />;
     },
 };
 
 export const Standard: Story = {
+    render: Template,
     args: {
         variant: 'standard',
-    },
-    render: ({ options, ...args }) => {
-        return <Select options={options} {...args} />;
     },
 };
 
 export const SmallSize: Story = {
+    render: Template,
     args: {
         sz: 'small',
-    },
-    render: ({ options, ...args }) => {
-        return <Select options={options} {...args} />;
     },
 };
 
 export const StandardSize: Story = {
+    render: Template,
     args: {
         sz: 'standard',
-    },
-    render: ({ options, ...args }) => {
-        return <Select options={options} {...args} />;
     },
 };
