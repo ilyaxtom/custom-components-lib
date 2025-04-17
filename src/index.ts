51570fea3d75aa9e43d2523d76cb3ed7e009eb99
ilyaxtom <ilyaganak5@gmail.com>
2025-04-17 16:59:01 +0300

    Prepare for upload Update gitignore with App.tsx, index.tsx Added index.ts for re-export components

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import Button from '@/components/Button';
+import Checkbox from '@/components/Checkbox';
+import Modal from '@/components/Modal';
+import Select from '@/components/Select';
+import Switch from '@/components/Switch';
+import TextField from '@/components/TextField';
+
+export { Button, Checkbox, Select, Modal, Switch, TextField };
